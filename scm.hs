1935306f66111fc1a13384ef95d8b9bf69ff47a1
lukasjoc <jochamlu@gmail.com>
2021-10-31 12:41:53 +0100

    basic

--- scm.hs
+++ scm.hs
@@ -1,9 +1,14 @@
 module Main where
 import System.Environment
+import Data.Int
 
 main :: IO()
 main = do
+
   args <- getArgs
 
-  putStrLn("Hello, " ++ args !! 0)
+  let a1 = read(args !! 0) :: Int32
+  let a2 = read(args !! 1) :: Int32
+  let result = (+) a1 a2
 
+  putStrLn(show a1 ++ "+" ++ show a2 ++ "=" ++ show result)
