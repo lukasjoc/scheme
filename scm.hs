79e45e904f738f76efdddcf6218ed5e090fd6530
lukasjoc <jochamlu@gmail.com>
2021-10-31 12:58:05 +0100

    funcs

--- scm.hs
+++ scm.hs
@@ -3,16 +3,14 @@ import System.Environment
 import Data.Int
 
 main :: IO()
-main = do
 
+sumAndShowResult :: Int32 -> Int32 -> String
+sumAndShowResult a b = show a ++ " + " ++ show b ++ " = " ++ show ((+) a b)
+
+main = do
   args <- getArgs
 
   let a1 = read(args !! 0) :: Int32
   let a2 = read(args !! 1) :: Int32
-  let result = (+) a1 a2
 
-  showresult :: Int32 -> Int32 -> Int32 -> String
-  showresult a b c = show a ++ " + " show b ++ " = " ++ show c
-
-  -- putStrLn(show a1 ++ "+" ++ show a2 ++ "=" ++ show result)
-  putStrLn(showresult)
+  putStrLn(sumAndShowResult a1 a2)
